a850c879dea30bb5e3699d0ed7b84102ea01011a
sreya519 <sreyanalla@gwu.edu>
2020-02-20 18:21:38 -0500

    very basic sample code completed with throughput calculation that prints to console

--- wiki/inflightWifiBlog/sampleCodePython/server.py
+++ wiki/inflightWifiBlog/sampleCodePython/server.py
@@ -2,6 +2,8 @@
 import socket      
 # import time module; use to track send/receive time
 import time 
+# import os; use to get file size 
+import os
 
 # Reserve a port
 port = 60000 
@@ -24,16 +26,30 @@ while True:
     #start time from when connection begins
     start = time.time()
 
+    # get file
     filename='test.txt'
+    # get file size (in bytes) and convert to bits
+    fsize = (os.path.getsize(filename))*8 #1 byte = 8 bits
+
+    #open file, read, send
     f = open(filename,'rb')
     l = f.read(1024)
     while (l):
        conn.send(l)
        print('Sent ',repr(l))
        l = f.read(1024)
+    # close file
     f.close()
-    print('Done sending')
-    runtime = time.time() - start;
-    print('total send time: %.2f ', runtime)
-    # close the connection between client and server
-    conn.close()
+    if f.closed:
+       break
+# calculate time taken to send given file
+runtime = time.time() - start;
+print('Done sending')
+print('total send time: %.2f ', runtime)
+# calculate and output throuhgput measured in bits/second
+print('throughput: %.2f ', fsize/runtime)
+
+# close the connection between client and server
+conn.close()
+print('server connection closed')
+
